957f9f0b157df27c2f595b2d7495f2a23f101930
Abinash Panda <abinash@prodios.com>
2020-08-16 23:20:09 +0530

    Fix bug in the client dockerfile

--- client/docker-entrypoint.sh
+++ client/docker-entrypoint.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
-set -ea
 
-if [ "$1" = "client" ]; then
- if [ ! -d "node_modules" ] || [ ! "$(ls -qAL node_modules 2>/dev/null)" ]; then
-    echo "Node modules not installed. Installing..."
-    if [ -f "yarn.lock" ]; then
-      yarn install
-    else
-      npm install
-    fi
-  fi
+if [ ! -d "node_modules" ] || [ ! "$(ls -qAL node_modules 2>/dev/null)" ]; then
+  echo "Node modules not installed. Installing..."
+  yarn install
 fi
 
 echo "Starting your app..."
