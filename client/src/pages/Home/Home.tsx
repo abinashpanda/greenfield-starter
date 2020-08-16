271855f3b4538b7555d65e4f143130ac215782f7
Abinash Panda <abinash@prodios.com>
2020-08-16 23:42:20 +0530

    Add logout button in Home page example

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,7 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import moment from 'moment'
+import { Button } from 'antd'
+import { LogoutOutlined } from '@ant-design/icons'
+import AuthContext from 'contexts/AuthContext'
 
 const Home = () => {
+  const { signOut } = useContext(AuthContext)
+
   const [now, setNow] = useState(moment())
 
   useEffect(() => {
@@ -15,7 +20,16 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="flex items-center justify-center w-full h-screen bg-gray-900">
+    <div className="relative flex items-center justify-center w-full h-screen bg-gray-900">
+      <Button
+        className="absolute top-0 right-0 m-4"
+        type="primary"
+        icon={<LogoutOutlined />}
+        onClick={signOut}
+      >
+        Signout
+      </Button>
+
       <div className="space-x-4 text-6xl font-semibold text-white text-num">
         <span>{now.format('HH:mm:ss')}</span>
         <span className="text-lg">{now.format('a').toUpperCase()}</span>
