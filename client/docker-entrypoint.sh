d1808d6bc36875f74b99063e90ceaa0fe4a78408
Abinash Panda <abinash@prodios.com>
2020-08-19 11:52:34 +0530

    Simplied docker-entrypoint

--- client/docker-entrypoint.sh
+++ client/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -d "node_modules" ] || [ ! "$(ls -qAL node_modules 2>/dev/null)" ]; then
+if [ ! -d "node_modules" ] || [ ! "$(ls -A node_modules)" ]; then
   echo "Node modules not installed. Installing..."
   yarn install
 fi
